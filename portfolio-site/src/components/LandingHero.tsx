d99b56764f7796d31b4862adcd8ad5d6bff20358
Arthur Riechert <arthur@cybrco.ai>
2023-12-03 01:29:46 -0500

    Added basic hero section

---
+++ portfolio-site/src/components/LandingHero.tsx
@@ -0,0 +1,14 @@
+export default function LandingHero() {
+    return (
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
+        <h1 className='text-5xl'>Software Engineer</h1>
+        <div className="flex">
+          <p className='m-5 text-xl leading-8'>
+            Hi! My name is Arthur. I am a computer science major and an 
+            aspiring software engineer currently attending 
+            Georgia Institute of Technology.
+          </p>
+        </div>
+      </div>
+    )
+}
