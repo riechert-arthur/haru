aa3d4ea47cd60934695010c3a6c886addd4b34a3
Arthur Riechert <arthur@cybrco.ai>
2023-12-03 17:58:12 -0500

    Moved button to outer div

--- portfolio-site/src/components/LandingHero.tsx
+++ portfolio-site/src/components/LandingHero.tsx
@@ -3,19 +3,21 @@ import Image from 'next/image'
 
 export default function LandingHero() {
     return (
-        <div className="flex z-10 w-full justify-between font-mono text-sm tracking-wider">
-            <div>
-                <h1 className='text-5xl'>Software Engineer</h1>
-                <div className="flex max-w-4xl">
-                    <p className='mt-14 text-2xl leading-8 tracking-widest leading-relaxed'>
-                        Hi, my name is Arthur. I am a computer science 
-                        student and aspiring software engineer from
-                        Georgia Institute of Technology.
-                    </p>
+        <div>
+            <div className="flex z-10 w-full justify-between items-center font-mono text-sm tracking-wider">
+                <div>
+                    <h1 className='text-5xl'>Software Engineer</h1>
+                    <div className="flex max-w-4xl">
+                        <p className='mt-14 text-2xl leading-8 tracking-widest leading-relaxed'>
+                            Hi, my name is Arthur. I am a computer science 
+                            student and aspiring software engineer from
+                            Georgia Institute of Technology.
+                        </p>
+                    </div>
                 </div>
-                <button className={`p-6 mt-14 bg-sky-500/80 rounded-2xl ${montserrat.className} font-bold text-3xl`}>Learn More</button>
+                <Image src='/head_shot.jpeg' width='300' height='300' alt='Picture of Arthur' className='ml-40 mr-12 w-96 h-96 bg-red-200 rounded-full'/>
             </div>
-            <Image src='/head_shot.jpeg' width='300' height='300' alt='Picture of Arthur' className='ml-40 mr-12 w-96 h-96 bg-red-200 rounded-full'/>
+            <button className={`p-6 bg-sky-500/80 rounded-2xl ${montserrat.className} font-bold text-3xl`}>Learn More</button>
         </div>
     )
 }
