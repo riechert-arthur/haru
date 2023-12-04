25adf8b3b48cc018953d231704385fdd066e8215
Arthur Riechert <arthur@cybrco.ai>
2023-12-04 02:44:21 -0500

    Added hovering effects for links

--- portfolio-site/src/components/NavBar.tsx
+++ portfolio-site/src/components/NavBar.tsx
@@ -15,11 +15,11 @@ const navigationLinks: NavigationLinks = {
 export default function NavBar() {
     return (
         <nav className='flex items-center justify-between px-11 py-4 bg-slate-900/60'>
-            <a href='/' className={`text-2xl tracking-wide ${montserrat.className}`}>Arthur Riechert</a>
+            <a href='/' className={`text-2xl tracking-wide ${montserrat.className} hover:text-gray-400 hover:scale-105`}>Arthur Riechert</a>
             <div className='flex items-center justify-between'>
                 {
                     Object.keys(navigationLinks).map((key) => (
-                        <a className='mr-7 text-xl tracking-wide' href={navigationLinks[key]}>{key}</a>
+                        <a className='mr-7 text-xl tracking-wide hover:text-gray-400' href={navigationLinks[key]}>{key}</a>
                     ))
                 }
                 <Button 
