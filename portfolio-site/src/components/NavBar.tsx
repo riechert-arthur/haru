dff655489872b55a62e9f3b6370005340ca8e3d2
Arthur Riechert <arthur@cybrco.ai>
2023-12-04 23:23:06 -0500

    Implemented router for resume button

--- portfolio-site/src/components/NavBar.tsx
+++ portfolio-site/src/components/NavBar.tsx
@@ -1,30 +1,38 @@
+'use client'
+
 import { montserrat } from './Fonts'
+import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 import Button from '../components/Button'
 
-interface NavigationLinks {
-    [key: string]: string;
-}
-
-const navigationLinks: NavigationLinks = {
-    About: "/about",
-    Projects: "/projects",
-    Experience: "/experience",
-    Contact: "/contact",
-}
-
 export default function NavBar() {
+
+    const router = useRouter()
+
+    interface NavigationLinks {
+        [key: string]: string;
+    }
+    
+    const navigationLinks: NavigationLinks = {
+        About: "/about",
+        Projects: "/projects",
+        Experience: "/experience",
+        Contact: "/contact",
+    }
+
     return (
         <nav className='flex items-center justify-between px-11 py-4 bg-slate-900/60'>
-            <a href='/' className={`text-2xl tracking-wide ${montserrat.className} hover:text-gray-400 hover:scale-105`}>Arthur Riechert</a>
+            <Link href='/' className={`text-2xl tracking-wide ${montserrat.className} hover:text-gray-400 hover:scale-105`}>Arthur Riechert</Link>
             <div className='flex items-center justify-between'>
                 {
                     Object.keys(navigationLinks).map((key) => (
-                        <a className='mr-7 text-xl tracking-wide hover:text-gray-400' href={navigationLinks[key]}>{key}</a>
+                        <Link className='mr-7 text-xl tracking-wide hover:text-gray-400' href={navigationLinks[key]}>{key}</Link>
                     ))
                 }
-                <Button 
+                <Button
                     text='Resumè'
                     extraClassNames='px-4 py-2 rounded-lg text-xl'
+                    functionToCall={() => router.push('/resume')}
                 />
             </div>
         </nav>
