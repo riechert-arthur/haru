e06de80631c384d461316de51523af3f9d731276
Arthur Riechert <arthur@cybrco.ai>
2023-12-05 23:09:06 -0500

    Corrected formatting for skills and added state for expanding skills

--- portfolio-site/src/components/SkillList.tsx
+++ portfolio-site/src/components/SkillList.tsx
@@ -1,9 +1,9 @@
 import { montserrat } from './Fonts'
 import Image from 'next/image'
+import { useState } from 'react'
 /**
  * TODO:
- * - Correct formatting on each skill.
- * - Add ability to expand boxes for more details.
+ * - Correct use of state to expand boxes.
  *
  * @author Arthur Riechert
  * @version 1.0.0
@@ -21,6 +21,7 @@ export default function SkillList() {
         Name: string
         IconPath: string
         Details: Details
+        Expanded: boolean
     }
 
     const skillDetails: SkillSelection[] = [
@@ -31,6 +32,7 @@ export default function SkillList() {
                 Libraries: ['Numpy', 'Pandas', 'Beautiful Soup', 'Streamlit'],
                 Frameworks: ['Flask', 'Django', 'FastAPI']
             },
+            Expanded: false
         },
         {
             Name: 'Typescript/Javascript',
@@ -38,42 +40,59 @@ export default function SkillList() {
             Details: {
                 Frameworks: ['React', 'Next', 'Node']
             },
+            Expanded: false
         },
         {
             Name: 'HTML/CSS',
             IconPath: 'html-5.svg',
             Details: {
                 Libraries: ['TailwindCSS']
-            }
+            },
+            Expanded: false
         },
         {
             Name: 'Java',
             IconPath: '/java.svg',
             Details: {
                 Frameworks: ['Spring']
-            }
+            },
+            Expanded: false
         },
         {
             Name: 'Developer Tools',
             IconPath: '/merge.svg',
             Details: {
                 Software: ['Git & Github']
-            }
+            },
+            Expanded: false
         }
     ]
+
+    const [skills, setSkills] = useState(skillDetails)
 
     return (
         <div className='w-full'>
             <text className={`${montserrat.className} text-xl tracking-wider font-light`}><i>Click each skill for more detail.</i></text>
             <div className='mt-7 grid grid-cols-2 gap-6 w-full'>
                 {
-                    skillDetails.map((skill: SkillSelection) => (
+                    skills.map((skill: SkillSelection, index: number) => (
                         <div className='relative p-11 items-center justify-between bg-slate-700 text-4xl font-mono rounded-xl hover:cursor-pointer hover:scale-105'>
-                            <div className='flex overflow-x-scroll'>
+                            <div className={`flex overflow-x-scroll ${skill.Expanded ? '' : 'h-20'}`}>
                                 <Image width='75' height='75' alt={`${skill.Name} icon.`} src={skill.IconPath} />
-                                <h2 className='ml-6 leading-loose'>{ skill.Name }</h2>
+                                <h2 className='ml-6 leading-loose translate-y-1'>{ skill.Name }</h2>
+                                <div className={`${skill.Expanded ? '' : 'hidden'}`}>
+                                    <h2>Hello</h2>
+                                </div>
                             </div>
-                            <button className='absolute right-1 bottom-1 -translate-x-1 -translate-y-1'>
+                            <button 
+                                className='absolute right-1 bottom-1 -translate-x-1 -translate-y-1'
+                                onClick={() => {
+                                    var temp: boolean = skills[index].Expanded
+                                    skills[index].Expanded = !temp
+                                    setSkills(skills)
+                                    console.log(skills[index].Expanded)
+                                }}
+                            >
                                 <Image width='15' height='15' alt='Expand' src='/arrow-button.svg'/>
                             </button>
                         </div>
