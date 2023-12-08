2f1c6325d18ab718f795f0c600ecc0cd2109c877
Arthur Riechert <arthur@cybrco.ai>
2023-12-08 13:26:15 -0500

    Refactored list of projects to separate component

--- portfolio-site/src/app/projects/page.tsx
+++ portfolio-site/src/app/projects/page.tsx
@@ -1,10 +1,6 @@
-'use client'
-
-import { montserrat } from '../../components/Fonts'
-import { useRouter } from 'next/navigation'
-import Button from '../../components/Button'
 import Link from 'next/link'
 import Image from 'next/image'
+import ProjectList from '../../components/ProjectList'
 
 /**
  * A page to store projects, so visitors can interact with them.
@@ -14,46 +10,10 @@ import Image from 'next/image'
  * - Add separate pages for each project.
  *
  * @author Arthur Riechert
- * @version 1.0.0
+ * @version 1.0.1
  */
 
-interface Project {
-    Title: string
-    Description: string
-    ReadMoreURL: string
-    ViewWebsiteURL: string
-}
-
 export default function Projects() {
-
-    const projects: Project[] = [
-        {
-            Title: 'Portfolio Site',
-            Description: 'A centralized website to house my qualifications and showcase my projects to potential clients and employers.',
-            ReadMoreURL: '',
-            ViewWebsiteURL: '',
-        },
-        {
-            Title: 'Abstract Data Type Library',
-            Description: 'A custom-built, idiomatic Java library for creating a variety of abstract data types such as linked lists and hash maps to use with sorting algorithms.',
-            ReadMoreURL: '',
-            ViewWebsiteURL: '',
-        },
-        {
-            Title: 'Portfolio Site',
-            Description: 'A centralized website to house my qualifications and showcase my projects to potential clients and employers.',
-            ReadMoreURL: '',
-            ViewWebsiteURL: '',
-        },
-        {
-            Title: 'Abstract Data Type Library',
-            Description: 'A custom-built, idiomatic Java library for creating a variety of abstract data types such as linked lists and hash maps to use with sorting algorithms.',
-            ReadMoreURL: '',
-            ViewWebsiteURL: '',
-        },
-    ]
-
-    const router = useRouter()
 
     return (
         <div>
@@ -62,33 +22,7 @@ export default function Projects() {
                 <text>Find more <Link href='https://github.com/riechert-arthur'><u>here</u></Link></text>
                 <Image src='github.svg' width='24' height='24' alt='Github icon'></Image>
             </div>
-            <div className='grid grid-cols-2 gap-6'>
-                {
-                    projects.map(project => (
-                        <div className={`${montserrat.className}`}>
-
-                            {/* Add Image up here. */}
-
-                            <h2>{ project.Title }</h2>
-                            <p>{ project.Description }</p>
-                            <div className='flex'>
-                                <Button
-                                    text='Read More'
-                                    extraClassNames='px-4 py-2 rounded-lg text-xl'
-                                    functionToCall={() => router.push(project.ReadMoreURL)}
-                                />
-                                <Button
-                                    text='View Website'
-                                    extraClassNames='px-4 py-2 rounded-lg text-xl'
-                                    functionToCall={() => router.push(project.ViewWebsiteURL)}
-                                >
-                                    <Image src='full-screen.svg' width='24' height='24' alt='Github icon'></Image>
-                                </Button>
-                            </div>
-                        </div>
-                    ))
-                }
-            </div>
+            <ProjectList />
         </div>
     )
 }
---
+++ portfolio-site/src/components/ProjectList.tsx
@@ -0,0 +1,75 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { montserrat } from './Fonts'
+import Button from './Button'
+import Image from 'next/image'
+
+interface Project {
+    Title: string
+    Description: string
+    ReadMoreURL: string
+    ViewWebsiteURL: string
+}
+
+export default function ProjectList({}) {
+
+    const router = useRouter()
+
+    const projects: Project[] = [
+        {
+            Title: 'Portfolio Site',
+            Description: 'A centralized website to house my qualifications and showcase my projects to potential clients and employers.',
+            ReadMoreURL: '',
+            ViewWebsiteURL: '',
+        },
+        {
+            Title: 'Abstract Data Type Library',
+            Description: 'A custom-built, idiomatic Java library for creating a variety of abstract data types such as linked lists and hash maps to use with sorting algorithms.',
+            ReadMoreURL: '',
+            ViewWebsiteURL: '',
+        },
+        {
+            Title: 'Portfolio Site',
+            Description: 'A centralized website to house my qualifications and showcase my projects to potential clients and employers.',
+            ReadMoreURL: '',
+            ViewWebsiteURL: '',
+        },
+        {
+            Title: 'Abstract Data Type Library',
+            Description: 'A custom-built, idiomatic Java library for creating a variety of abstract data types such as linked lists and hash maps to use with sorting algorithms.',
+            ReadMoreURL: '',
+            ViewWebsiteURL: '',
+        },
+    ]
+
+    return (
+        <div className='grid grid-cols-2 gap-6'>
+            {
+                projects.map(project => (
+                    <div className={`${montserrat.className}`}>
+
+                        {/* Add Image up here. */}
+
+                        <h2>{ project.Title }</h2>
+                        <p>{ project.Description }</p>
+                        <div className='flex'>
+                            <Button
+                                text='Read More'
+                                extraClassNames='px-4 py-2 rounded-lg text-xl'
+                                functionToCall={() => router.push(project.ReadMoreURL)}
+                            />
+                            <Button
+                                text='View Website'
+                                extraClassNames='px-4 py-2 rounded-lg text-xl'
+                                functionToCall={() => router.push(project.ViewWebsiteURL)}
+                            >
+                                <Image src='full-screen.svg' width='24' height='24' alt='Github icon'></Image>
+                            </Button>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
