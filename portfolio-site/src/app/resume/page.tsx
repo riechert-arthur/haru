027023256ab7692cd26bda5275ba0f98bea77e9d
Arthur Riechert <arthur@cybrco.ai>
2024-01-02 14:12:21 -0500

    Added mobile compatability

--- portfolio-site/src/app/resume/page.tsx
+++ portfolio-site/src/app/resume/page.tsx
@@ -1,12 +1,39 @@
+import { montserrat } from "@/components/Fonts"
+
 /**
  * @author Arthur Riechert
  * @version 1.0.0
  */
+
 export default function Resume() {
     return (
-        <main className='px-48 py-48'>
-            <h2 className='text-6xl font-mono'>Resumè</h2>
-            <iframe className='mt-16 shadow-md' src='/resume.pdf' width='100%' height='1920'></iframe>
-        </main>
+        <>
+            <main className='hidden md:block px-48 py-48 mb-8'>
+                <h2 className='text-6xl font-mono'>Resumè</h2>
+                <div className='flex justify-center'>
+                    <iframe
+                        className='mt-16 shadow-md aspect-[8.5/11]'
+                        src='/resume.pdf#zoom=135'
+                        width='100%'
+                    >
+                    </iframe>
+                </div>
+            </main>
+            <div className='flex flex-col py-8 items-center md:hidden'>
+                <h2 className='text-6xl font-mono'>Resumè</h2>
+                {/* Render a link to download the PDF on smaller screens */}
+                <a
+                    className={`flex items-center justify-center 
+                        w-3/5 p-8 mt-12 text-3xl bg-sky-500/80 hover:bg-sky-600/80
+                        font-semibold tracking-wide transition-opacity rounded-3xl
+                        ${montserrat.className}`}
+                    download="riechert-resume.pdf"
+                    href='/resume.pdf'
+                >
+                    <text className='w-fit'>Download</text>
+                </a>
+            </div>
+            <div className='h-72'></div>
+        </>
     )
 }
