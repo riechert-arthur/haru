d901561f1f600b44f497233fcf079c8562cb8a12
Arthur Riechert <arthur@cybrco.ai>
2024-01-05 00:37:14 -0500

    Adjusted widths and heights

--- portfolio-site/src/app/page.tsx
+++ portfolio-site/src/app/page.tsx
@@ -7,7 +7,7 @@ import LandingHero from "../components/LandingHero"
 export default function Home() {
   return (
     <main className={ `flex justify-center w-full overflow-x-hidden min-h-screen px-4 md:max-lg:px-16 md:px-32 py-20
-      md:max-lg:py-30 md:max-lg:mb-24 md:py-60 md:bg-polygon-scatter md:bg-cover 
+      md:max-lg:py-30 md:max-lg:mb-24 md:pt-30 md:pb-60 md:bg-polygon-scatter md:bg-cover 
       md:bg-no-repeat md:bg-[right_-42rem_top_-3rem]`
     }>
       <LandingHero></LandingHero>
--- portfolio-site/src/components/LandingHero.tsx
+++ portfolio-site/src/components/LandingHero.tsx
@@ -15,7 +15,7 @@ export default function LandingHero() {
 
     return (
         <div className='lg:max-w-large-screens flex flex-col items-center w-full'>
-            <div className={`flex flex-col mb-8 md:flex-row w-full md:justify-between
+            <div className={`flex flex-col mb-8 lg:mb-0 md:flex-row w-full md:justify-between
                 items-center font-mono text-sm tracking-wider`
             }>
                 <div className='flex flex-col w-full justify-center items-center'>
@@ -28,7 +28,7 @@ export default function LandingHero() {
                         className='mt-8 md:hidden md:ml-40 md:mr-12 w-64 h-64 bg-red-200 rounded-full shadow-2xl'
                     />
                     <div className="flex max-w-4xl">
-                        <p className='mt-14 text-2xl text-center md:text-left leading-8 tracking-widest leading-relaxed'>
+                        <p className='lg:mt-7 mt-14 text-2xl text-center md:text-left leading-8 tracking-widest leading-relaxed'>
                             Hi, my name is Arthur. I am a computer science 
                             student and aspiring software engineer from
                             Georgia Institute of Technology.
@@ -45,7 +45,7 @@ export default function LandingHero() {
             </div>
             <Button
                 text='Learn More'
-                extraClassNames='md:self-start md:mt-0 p-6 w-2/3 md:w-fit text-3xl rounded-2xl'
+                extraClassNames='md:self-start lg:mt-0 p-6 w-2/3 md:w-fit text-3xl rounded-2xl'
                 functionToCall={() => router.push(learnMorePath)}
             />
         </div>
