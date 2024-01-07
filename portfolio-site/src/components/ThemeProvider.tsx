f067a9831ef725ad907f82a5f5dd9967039c516d
Arthur Riechert <arthur@cybrco.ai>
2024-01-07 15:44:51 -0500

    Added context provider to manage dark mode theme

---
+++ portfolio-site/src/components/ThemeProvider.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { createContext, useState } from "react"
+
+/**
+ * Uses context to control dark & light mode. 
+ *
+ * @author Arthur Riechert
+ * @version 1.0.0
+ */
+
+const ThemeContext = createContext({
+    darkMode: false,
+    toggleDarkMode: () => {},
+})
+
+export default function ThemeProvider({ children}: { children: any }) {
+
+    const [darkMode, setDarkMode] = useState(false)
+
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode)
+    }
+
+    const contextValue = {
+        darkMode,
+        toggleDarkMode
+    }
+
+    return (
+        <ThemeContext.Provider value={ contextValue }>
+            { children }
+        </ThemeContext.Provider>
+    )
+}
